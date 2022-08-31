5433f10349ff5410e5ab3c44b63990566f0350f8
CJ Harries <charries@nexient.com>
2022-08-31 09:50:30 -0500

    Pull in existing TF

--- main.tf
+++ main.tf
@@ -12,8 +12,135 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-resource "random_string" "string" {
-  length  = var.length
-  number  = var.number
-  special = var.special
+data "aws_caller_identity" "current" {}
+
+###############################################################################
+# IAM
+###############################################################################
+resource "aws_iam_role" "ecs_task_assume" {
+  name = "${var.name}-ecs-task-assume-000"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com",
+        "AWS": [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "${var.name}-ecs-task-role-000"
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "${var.name}_ecs_task_role"
+    },
+  )
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com",
+        "AWS": [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy" "ecs_default_task_role" {
+  name = "${var.name}-ecs-default-task-role-000"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "${aws_cloudwatch_log_group.task_logs.arn}"
+        }
+    ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.ecs_task_assume.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+###############################################################################
+# Task Logs
+###############################################################################
+resource "aws_cloudwatch_log_group" "task_logs" {
+  name = var.log_group_name
+
+  retention_in_days = var.log_retention_days
+  kms_key_id        = var.kms_key_id
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.log_group_name
+    },
+  )
+}
+
+###############################################################################
+# ECS Service/Task
+###############################################################################
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = var.name
+  network_mode             = var.network_mode
+  requires_compatibilities = [var.requires_compatibilities]
+  execution_role_arn       = aws_iam_role.ecs_task_assume.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  cpu                      = var.cpu
+  memory                   = var.memory
+  container_definitions    = var.task_definition
+  tags = merge(
+    var.tags,
+    {
+      "Name" = var.name
+    },
+  )
 }
--- variables.tf
+++ variables.tf
@@ -12,17 +12,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-variable "length" {
-  type    = number
-  default = 24
+variable "name" {
+  description = "Application Name running in ECS Task"
+  type        = string
 }
 
-variable "number" {
-  type    = bool
-  default = true
+variable "cpu" {
+  description = "Task CPU units to provision (1 vCPU = 1024 CPU units)"
+  type        = string
+  default     = "256"
 }
 
-variable "special" {
-  type    = bool
-  default = false
+variable "memory" {
+  description = "Task memory to provision (in MB)"
+  type        = string
+  default     = "512"
+}
+
+variable "task_definition" {
+  description = "Task Definition to create"
+  type        = string
+}
+
+variable "network_mode" {
+  description = "Set which network mode to use"
+  type        = string
+  default     = "awsvpc"
+}
+
+variable "requires_compatibilities" {
+  description = "Set FARGATE or EC2 Requirement"
+  type        = string
+  default     = "FARGATE"
+}
+
+variable "log_group_name" {
+  description = "Name of the log group to create for task logs"
+  type        = string
+}
+
+variable "log_retention_days" {
+  description = "Set how many days you wish to retain logs"
+  type        = string
+  default     = "7"
+}
+
+variable "kms_key_id" {
+  description = "Optional: Set a KMS Key ID to encrypt logs"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to be applied to all resources created"
+  type        = map(string)
+  default     = {}
 }
